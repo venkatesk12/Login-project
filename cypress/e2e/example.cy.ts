bfb608b0cbb6cc62e88b44fa8011a573ab1b99c7
venkatesk12 <venkatesh.k@innorat.io>
2024-05-20 16:47:53 +0530

    cypress add

--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -1,6 +1,6 @@
 describe('My Test', () => {
     it('passes', () => {
-      cy.visit('https://www.google.com');
-      cy.get('h1').should('contain', 'Example Domain');
+      cy.visit('http://localhost:4200/');
+      
     });
   }); 
