a3e1977086865d28fa0b60903d1e9222606cad40
venkatesk12 <venkatesh.k@innorat.io>
2024-05-20 13:04:20 +0530

    cypress file addded

--- karma.conf.js
+++ karma.conf.js
@@ -1,19 +1,50 @@
-module.exports = function(config) {
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/1.0/config/configuration-file.html
+
+module.exports = function (config) {
     config.set({
-      // other configuration options
-      logLevel: config.LOG_DEBUG,
-      browsers: ['ChromeHeadless'],
-      singleRun: true,
-      frameworks: ['jasmine'],
-      files: [
-        // specify your test files here
-      ],
+      basePath: '',
+      frameworks: ['jasmine', '@angular-devkit/build-angular'],
       plugins: [
-        'karma-chrome-launcher',
-        'karma-jasmine',
-        'karma-spec-reporter'
+        require('karma-jasmine'),
+        require('karma-chrome-launcher'),
+        require('karma-jasmine-html-reporter'),
+        require('karma-coverage'),
+        require('@angular-devkit/build-angular/plugins/karma')
       ],
-      reporters: ['spec'],
+      client: {
+        jasmine: {
+          // you can add configuration options for Jasmine here
+          // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
+          // for example, you can disable the random execution with `random: false`
+          // or set a specific seed with `seed: 4321`
+        },
+        clearContext: false // leave Jasmine Spec Runner output visible in browser
+      },
+      jasmineHtmlReporter: {
+        suppressAll: true // removes the duplicated traces
+      },
+      coverageReporter: {
+        dir: require('path').join(__dirname, './coverage/quizAngular'),
+        subdir: '.',
+        reporters: [
+          { type: 'html' },
+          { type: 'text-summary' }
+        ]
+      },
+      reporters: ['progress', 'kjhtml'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+      customLaunchers: {
+       ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+       flags: ['--no-sandbox']
+    }
+  },
+      singleRun: false,
+      restartOnFileChange: true
     });
   };
-  
