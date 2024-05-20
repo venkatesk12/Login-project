be3edfa47b2ca7e52c947ef0bc56da8537f47e6b
venkatesk12 <venkatesh.k@innorat.io>
2024-05-20 15:04:56 +0530

    cypress add

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
         suppressAll: true // removes the duplicated traces
       },
       coverageReporter: {
-        dir: require('path').join(__dirname, './coverage/uswdsscss'),
+        dir: require('path').join(__dirname, './coverage/frond-end'),
         subdir: '.',
         reporters: [
           { type: 'html' },
