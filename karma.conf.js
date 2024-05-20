1ffe7d2fb01f7ad26f4a27a9c89e15ad1f94276f
venkatesk12 <venkatesh.k@innorat.io>
2024-05-20 13:01:26 +0530

    cypress file addded

--- karma.conf.js
+++ karma.conf.js
@@ -1,7 +1,19 @@
 module.exports = function(config) {
     config.set({
-      browsers: ['ChromeHeadless'],
       // other configuration options
+      logLevel: config.LOG_DEBUG,
+      browsers: ['ChromeHeadless'],
+      singleRun: true,
+      frameworks: ['jasmine'],
+      files: [
+        // specify your test files here
+      ],
+      plugins: [
+        'karma-chrome-launcher',
+        'karma-jasmine',
+        'karma-spec-reporter'
+      ],
+      reporters: ['spec'],
     });
   };
   
