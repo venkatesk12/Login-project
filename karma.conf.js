72f79ca0d3226b180a869b6478ea3bd0df526389
venkatesk12 <venkatesh.k@innorat.io>
2024-05-20 13:13:00 +0530

    cypress file addded

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
         suppressAll: true // removes the duplicated traces
       },
       coverageReporter: {
-        dir: require('path').join(__dirname, './coverage/uswdsscss'),
+        dir: require('path').join(__dirname, './coverage/frond-end'),
         subdir: '.',
         reporters: [
           { type: 'html' },
@@ -37,9 +37,14 @@ module.exports = function (config) {
       colors: true,
       logLevel: config.LOG_INFO,
       autoWatch: true,
-      browsers: ['Chrome'],
+      browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessCI'],
+      customLaunchers: {
+       ChromeHeadlessCI: {
+        base: 'ChromeHeadless',
+       flags: ['--no-sandbox']
+    }
+  },
       singleRun: false,
       restartOnFileChange: true
     });
   };
-  
